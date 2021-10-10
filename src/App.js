546ae96fb22dd710fb9fe498a393be35e8066b46
kiwoon kim <kisoon0947@gmail.com>
2021-10-10 23:02:59 +0900

    update

--- src/App.js
+++ src/App.js
@@ -31,6 +31,25 @@ function App() {
               <Button variant="primary">Shop Now</Button>
             </p>
         </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <img src="https://www.fmicassets.com/Damroot/GuitarVertDesktopJpg/10001/0140262326_fen_ins_frt_1_rr.jpg" height="70%" alt="" />
+              <h4>상품명</h4>
+              <p>상품설명 & 가격</p>
+            </div>
+            <div className="col-md-4">
+              <img src="https://www.fmicassets.com/Damroot/GuitarVertDesktopJpg/10001/0378051566_sqr_ins_frt_1_rr.jpg" height="70%" alt="" />
+              <h4>상품명</h4>
+              <p>상품설명 & 가격</p>
+            </div>
+            <div className="col-md-4">
+              <img src="https://www.fmicassets.com/Damroot/GuitarVertDesktopJpg/10001/0378003502_sqr_ins_frt_1_rr.jpg" height="70%" alt="" />
+              <h4>상품명</h4>
+              <p>상품설명 & 가격</p>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
