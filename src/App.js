2aa927121b0a3f00492cda267b2880849f41d1a6
kiwoon kim <kisoon0947@gmail.com>
2021-10-11 16:07:26 +0900

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import './App.css';
 import { Navbar, Container, Nav, NavDropdown, Button }  from 'react-bootstrap'
 import Data from './data'
+import Details from './Details'
+import Card from './Card'
 
 import { Link, Route, Switch } from 'react-router-dom'
 function App() {
@@ -16,8 +18,8 @@ function App() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
+                <Nav.Link><Link to="/">Home</Link></Nav.Link>
+                <Nav.Link><Link to="/detail">Detail</Link></Nav.Link>
                 <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                   <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                   <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -29,6 +31,7 @@ function App() {
             </Navbar.Collapse>
           </Container>
         </Navbar>
+        <Switch>
         <Route exact path="/">
         <div className="jumbotron">
             <h1>20% SALE</h1>
@@ -50,33 +53,12 @@ function App() {
         </div>
         </Route>
         <Route path="/detail">
-          <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-            </div>
-            <div className="col-md-6 mt-4">
-              <h4 className="pt-5">상품명</h4>
-              <p>상품설명</p>
-              <p>120000원</p>
-              <button className="btn btn-danger">주문하기</button> 
-            </div>
-          </div>
-      </div> 
+          <Details></Details>
         </Route>
+        </Switch> 
     </div>
   )
 }
 
-const Card = (props) =>
-{
-  return(
-        <div className="col-md-4">
-          <img src={props.guitar.src} height="70%" alt="팬더기타" />
-          <h4>{props.guitar.title}</h4>
-          <p>{props.guitar.content}</p>
-          <p><strong>{props.guitar.price}</strong></p>
-        </div>
-  )
-}
+
   export default App;
