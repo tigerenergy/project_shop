d948504eac40cf95a517c66a01e0a5b6fe0cefae
kiwoon kim <kisoon0947@gmail.com>
2021-10-27 11:01:35 +0900

    update

--- src/Cart.js
+++ src/Cart.js
@@ -1,31 +1,36 @@
 import React from 'react'
 import { Table } from 'react-bootstrap'
-import { connect } from 'react-redux'
+import { connect, useSelector, useDispatch } from 'react-redux'
 
 const Cart = (props) =>
-{
+{   
+
+    let state = useSelector((state)=> state)
+    let dispatch = useDispatch()
+
+
     return(
-        <div>
-  <Table responsive="sm">
-    <thead>
-      <tr>
-        <th>#</th>
-        <th>상품명</th>
-        <th>수량</th>
-        <th>변경</th>
-      </tr>
-    </thead>
-    <tbody>
-    {
-        props.state.map((item , key) =>
+  <div>
+    <Table responsive="sm">
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>상품명</th>
+            <th>수량</th>
+            <th>변경</th>
+        </tr>
+        </thead>
+        <tbody>
+        {
+        state.reducer.map((item , key) =>
         {
             return(
                 <tr key={key}>
                     <td>{item.id}</td>
                     <td>{item.name}</td>
                     <td>{item.quan}</td>
-                    <button onClick={()=>{props.dispatch({type:'add'})}}>+</button>
-                    <button onClick={()=>{props.dispatch({type:'remove'})}}>-</button>
+                    <button onClick={()=>{dispatch({type:'add'})}}>+</button>
+                    <button onClick={()=>{dispatch({type:'remove'})}}>-</button>
                 </tr>
             )
         })
@@ -37,7 +42,7 @@ const Cart = (props) =>
         props.alertOpen === true
         ? (<div className="alert">
             <p>지금 구매하시면 신규할인 20%</p>
-            <button onClick={()=>{props.dispatch({type:'close'})}}>닫기</button>
+            <button onClick={()=>{dispatch({type:'close'})}}>닫기</button>
             </div>)
         : null
         }
@@ -45,12 +50,14 @@ const Cart = (props) =>
             )
         }
 
-        const userCart = (state) =>
-        {
-            return {
-                state: state.reducer,
-                alertOpen : state.alertReducer
-            }
-        }
+        // const userCart = (state) =>
+        // {
+        //     return {
+        //         state: state.reducer,
+        //         alertOpen : state.alertReducer
+        //     }
+        // }
 
-        export default connect(userCart)(Cart)
+        // export default connect(userCart)(Cart)
+
+        export default Cart
